45de975c59b03ce169ea9f314cc345cd08667850
Jingguo Yao <yaojingguo@gmail.com>
2022-04-18 18:53:22 +0800

    update

--- absl-code/src/hello_world.cc
+++ absl-code/src/hello_world.cc
@@ -2,8 +2,8 @@
 #include <string>
 #include <vector>
 
-// #include "absl/container/flat_hash_map.h"
-// #include "absl/strings/str_format.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/str_format.h"
 #include "absl/strings/str_join.h"
 
 void Join() {
@@ -12,17 +12,17 @@ void Join() {
   std::cout << "Joined string: " << s << "\n";
 }
 
-// void Format() {
-//   std::string s =
-//       absl::StrFormat("Welcome to %s, Number %d!", "The Village", 6);
-//   std::cout << s << std::endl;
-// }
+void Format() {
+  std::string s =
+      absl::StrFormat("Welcome to %s, Number %d!", "The Village", 6);
+  std::cout << s << std::endl;
+}
 
-// void Container() {
-//   absl::flat_hash_map<int, std::string> map = {{10, "one"}, {20, "two"}};
-//   std::cout << map.contains(10) << std::endl;
-//   std::cout << map.contains(100) << std::endl;
-// }
+void Container() {
+  absl::flat_hash_map<int, std::string> map = {{10, "one"}, {20, "two"}};
+  std::cout << map.contains(10) << std::endl;
+  std::cout << map.contains(100) << std::endl;
+}
 
 int main() {
   Join();
