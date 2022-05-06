dba83a152b43e4befd16296cf239c33034498f60
Jingguo Yao <yaojingguo@gmail.com>
2022-05-06 15:13:20 +0800

    update

---
+++ absl-code/src/absl_test.cc
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(MySuite, MyTest) {
+  EXPECT_EQ(1 + 1, 2); 
+}
--- absl-code/src/glog_usage.cc
+++ absl-code/src/glog_usage.cc
@@ -2,5 +2,12 @@
 
 int main(int argc, char* argv[]) {
     google::InitGoogleLogging(argv[0]);
+
     LOG(INFO) << "Found " << 10 << " cookies";
+    LOG(ERROR) << "an error message";
+
+    // vlog
+    VLOG(0) << "vlog 0 message";
+    VLOG(1) << "vlog 1 message";
+    VLOG(2) << "vlog 2 message";
 }
