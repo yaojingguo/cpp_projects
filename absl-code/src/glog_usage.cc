6366266962c7928f0d01767a924342e3a52fa184
Jingguo Yao <yaojingguo@gmail.com>
2022-05-06 13:36:10 +0800

    update

--- absl-code/src/glog_usage.cc
+++ absl-code/src/glog_usage.cc
@@ -1,8 +1,6 @@
 #include "glog/logging.h"
 
 int main(int argc, char* argv[]) {
-    // Initialize Google’s logging library.
     google::InitGoogleLogging(argv[0]);
-
     LOG(INFO) << "Found " << 10 << " cookies";
 }
